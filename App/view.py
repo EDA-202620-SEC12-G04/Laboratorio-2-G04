75c8661d8974d54d232536388226dbdcd5478fe3
David Santiago Gracia Arevalo <d.graciaa@uniandes.edu.co>
2026-08-13 19:12:35 -0500

    cambios del Estudiante 3

--- App/view.py
+++ App/view.py
@@ -58,7 +58,6 @@ def print_menu():
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
     # Agregue la opción 3 para cargar los tags de los libros.
     # Pueede guiarse de las opciones 1 y 2.
-    print("3- Cargar Book-Tags!!!...")
     print("0- Salir")
 
 
