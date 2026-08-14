a92a84f22c299e1f047e6888f556c8e56d265d53
David Santiago Gracia Arevalo <d.graciaa@uniandes.edu.co>
2026-08-13 19:13:14 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -56,8 +56,7 @@ def print_menu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
+    print("3- Cargar los Tags de Libros")
     print("0- Salir")
 
 
